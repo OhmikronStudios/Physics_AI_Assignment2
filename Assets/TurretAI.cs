561ab8e6347f232a28e8920ee410eb1e738e0fe6
Joshua Smith <55558076+OhmikronStudios@users.noreply.github.com>
2020-11-15 20:03:19 -0500

    Second Push

---
+++ Assets/TurretAI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretAI : MonoBehaviour
+{
+    public float viewRadius;
+    [Range(0, 360)] public float viewAngle;
+
+    public GameObject seekObject;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+    public bool TargetIsVisible()
+    {
+        Vector3 dirToTarget = (seekObject.transform.position - transform.position).normalized;
+        float dstToTarget = Vector3.Distance(transform.position, seekObject.transform.position);
+        return (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2 && dstToTarget < viewRadius);
+    }
+
+    public bool Attack()
+    {
+        //Vector3 dirToTarget = (seekObject.transform.position - transform.position).normalized;
+        float dstToTarget = Vector3.Distance(transform.position, seekObject.transform.position);
+        return (dstToTarget < viewRadius / 3);
+
+    }
+}
---
+++ Assets/TurretTrack.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretTrack : StateMachineBehaviour
+{
+    float timer;
+    float timeToFire = 3.0f;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        timer = 0.0f;
+        animator.ResetTrigger("TrackingState");
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        timer += Time.deltaTime;
+        TurretAI script = animator.GetComponent<TurretAI>();
+        script.transform.LookAt(script.seekObject.transform);
+        if (timer >= timeToFire)
+        {
+            FindObjectOfType<KinematicInput>().HealthChange(5);
+            timer = 0.0f;
+        }
+        else if (!script.TargetIsVisible())
+        {
+            animator.SetTrigger("SearchingState");
+            timer = 0.0f;
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
